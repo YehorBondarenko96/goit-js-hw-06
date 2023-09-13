44706b1119993c19399c02662e8513731c1f7141
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-09-13 16:11:41 +0300

    51

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,12 @@
 const formText = document.querySelector('#name-input');
 const massageText = document.querySelector('#name-output');
 const copyName = (event) => {
-    massageText.textContent = event.currentTarget.value;
+    if (formText.value === "") {
+        
+        massageText.textContent = 'Anonymous';
+    } else {
+        massageText.textContent = event.currentTarget.value;
+    }
 };
-formText.addEventListener("input", copyName);
+    formText.addEventListener("input", copyName);
+
