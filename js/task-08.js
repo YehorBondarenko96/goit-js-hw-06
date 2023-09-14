3b74d8d39893091111dc371b4d56faa2c384be99
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-09-14 12:27:12 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
+const formData = {};
+function submission(event) {
+    event.preventDefault();
+    const input = event.currentTarget;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
 
+    if (email === '' || password === '') {
+        alert('All forms must be completed!');
+    }
+
+    formData.email = email;
+    formData.password = password;
+    console.log(formData);
+    form.reset();
+};
+form.addEventListener('submit', submission)
