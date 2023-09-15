6f7456d5de7ca8b66461be8cacc18e31b9df43ae
YehorBondarenko96 <egorbondarenko1996@gmail.com>
2023-09-15 16:02:19 +0300

    12

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,11 @@
 const listCategories = document.querySelector("#categories");
-const categories = listCategories.querySelectorAll(".item");
+const categories = listCategories.children;
 const numberCategories = categories.length;
 console.log('Number of categories:' + numberCategories);
-categories.forEach((category) => {
-    const titleCategories = category.querySelector("h2");
+
+Array.from(categories).forEach((category) => {
+    const titleCategories = category.firstElementChild;
     console.log('Category: ' + titleCategories.textContent);
-    const itemsCategory = category.querySelectorAll("li");
+    const itemsCategory = category.lastElementChild.children;
     console.log('Elements: ' + itemsCategory.length);
-});
+    });
